3ff3c3f981d5e5b137c84ac3037fb1930a08dd62
manuel astudillo <clerigo.morpheus@gmail.com>
2016-09-29 16:56:23 -0300

    Suviendo repositorio con administrador

---
+++ app/controllers/bienvenido_controller.rb
@@ -0,0 +1,8 @@
+class BienvenidoController < ApplicationController
+
+def index
+
+end 
+
+end
+
---
+++ app/models/ability.rb
@@ -0,0 +1,42 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+
+
+        can :read, :all                   # Permite leer toda la información
+    if user && user.admin?
+      can :access, :rails_admin       # Sólo el admin accede a Rails Admin
+      can :dashboard                  # Permite ver el dashboard
+      can :manage, :all               # Permite al administrador hacer todo (actualizar, eliminar..)
+    end
+    
+    # Define abilities for the passed in user here. For example:
+    #
+    #   user ||= User.new # guest user (not logged in)
+    #   if user.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the user 
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on. 
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+  end
+end
